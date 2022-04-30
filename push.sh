3866d50d71a7b51c1c2500c88750bbf2d2e07062
Alfonso Taboada <avtwarmerdam@gmail.com>
2022-04-30 20:05:36 +0200

    minor rearangements

--- push.sh
+++ push.sh
@@ -1,1 +1,9 @@
-/home/ook/proj/fashion/push.sh
+#!/bin/bash
+if [ "$#" -eq  "0" ]
+  then
+    echo "No arguments supplied"
+else
+    rsync --links -r --exclude-from=.rsyncignore -e ssh --delete . lisa2:$1
+fi
+
+
