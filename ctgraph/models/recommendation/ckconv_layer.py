779f7c8c4d9faec1ed03e5c9689dc09c4ce51d05
Lodewijk <lloerakker@gmail.com>
2022-05-21 15:03:30 +0200

    CKConv Factorized

--- ctgraph/models/recommendation/ckconv_layer.py
+++ ctgraph/models/recommendation/ckconv_layer.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from .dgsr_utils import sparse_dense_mul, pass_messages, relative_order, get_last
 
-from .ckconv_kernel import SirenKernel
+from .ckconv_kernel import SirenLibraryKernel, SirenCustomKernel
 
 class CKConv(nn.Module): # Continuous Kernel Convolution
     def __init__(self,
@@ -14,16 +14,37 @@ class CKConv(nn.Module): # Continuous Kernel Convolution
 
         self.hidden_size = hidden_size
         
-        self.siren_items
-        self.w2 = nn.Linear(self.hidden_size, self.hidden_size, bias=False) # User Messages
+        # Kernel settings
+        in_size = 1
+        out_size = hidden_size*hidden_size
+        layer_sizes = [hidden_size, hidden_size]
+        omega_0 = 30
+        bias = False
+
+        self.w_items = SirenLibraryKernel(in_size, out_size, layer_sizes, omega_0, bias)
+        self.w_users = SirenLibraryKernel(in_size, out_size, layer_sizes, omega_0, bias)
 
 
+    def forward(self, u_embedded, i_embedded, user_per_trans, item_per_trans, edges_t):
 
-    def forward(self, u_embedded, i_embedded, edges, edges_t):
+        # make times relative
+        newest_time = edges_t.max()
+        rel_time = newest_time - edges_t
 
-        hLu = self.w2(u_embedded)
-        hLi = self.w1(i_embedded)
+        # get kernels
+        user_kernels = self.w_users(rel_time).reshape((len(edges_t), self.hidden_size, self.hidden_size))
+        item_kernels = self.w_items(rel_time).reshape((len(edges_t), self.hidden_size, self.hidden_size))
+
+        # propagate item messages to user embeddings
+        item_messages = (item_kernels @ i_embedded[item_per_trans].unsqueeze(-1)).squeeze()
+
+        hLu = torch.zeros_like(u_embedded)
+        hLu.index_add_(0, user_per_trans, item_messages)
+
+        # propagate user messages to item embeddings
+        user_messages = (user_kernels @ i_embedded[user_per_trans].unsqueeze(-1)).squeeze()
+
+        hLi = torch.zeros_like(i_embedded)
+        hLi.index_add_(0, item_per_trans, user_messages)
 
         return hLu, hLi
-
-
